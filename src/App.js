c9fc24235e78b84dd8c1806dd270b379d3c4c6dd
Jonathan <jkd6735@gmail.com>
2021-07-21 22:40:44 +0000

    commit to main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <p>I want it show in the main file</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function Jonathan ({library, message, number}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>It's me, Jonathan</h1>
+      <p>I am going to be the best developer in the world</p>
+      <h2>Jonathan is currently learning {library}</h2>
+      <p>{message} at {number}</p>
+      
     </div>
   );
 }
 
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <Jonathan library = "React" message = "Have fun" number = {5}/>
+  </React.StrictMode>
+  ,
+  document.getElementById('root')
+);
+
+export default Jonathan;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Jonathan from "./App"
 
-function Jonathan (
-  {library, 
-    message, 
-    number}) {
-  return (
-    <div>
-      <h1>It's me, Jonathan</h1>
-      <p>I am going to be the best developer in the world</p>
-      <h2>Jonathan is currently learning {library}</h2>
-      <p>{message} at {number}</p>
-    </div>
-  )
-}
+
+
+// function Jonathan (
+//   {library, 
+//     message, 
+//     number}) {
+//   return (
+//     <div>
+//       <h1>It's me, Jonathan</h1>
+//       <p>I am going to be the best developer in the world</p>
+//       <h2>Jonathan is currently learning {library}</h2>
+//       <p>{message} at {number}</p>
+//       <p>Prince, this doesn't work for me at all</p>
+//       <p>Prince, is learning but he is doing his self like he is not doing anything</p>
+//     </div>
+//   )
+// }
 
 
 ReactDOM.render(
-  <Jonathan library = "React" message = "Have fun" number = {5}/>,
+  <React.StrictMode>
+    <Jonathan/>
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Jonathan library = "React" message = "Have fun" number = {5}/>
+//   </React.StrictMode>
+//   ,
+//   document.getElementById('root')
+// );
