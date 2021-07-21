ba33326f05e49699854ae57b63b0ba25e8509064
Kwaame Ofori-Adjekum <44780028+qwami-vibes@users.noreply.github.com>
2021-07-21 23:12:56 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Jonathan from "./App"
-
-
-
-// function Jonathan (
-//   {library, 
-//     message, 
-//     number}) {
-//   return (
-//     <div>
-//       <h1>It's me, Jonathan</h1>
-//       <p>I am going to be the best developer in the world</p>
-//       <h2>Jonathan is currently learning {library}</h2>
-//       <p>{message} at {number}</p>
-//       <p>Prince, this doesn't work for me at all</p>
-//       <p>Prince, is learning but he is doing his self like he is not doing anything</p>
-//     </div>
-//   )
-// }
+import App from "./App"
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Jonathan/>
+    <App message="Hey there", library="this library" number=4/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Jonathan library = "React" message = "Have fun" number = {5}/>
-//   </React.StrictMode>
-//   ,
-//   document.getElementById('root')
-// );
