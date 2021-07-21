e1eb9a6b8b297b30fdf89c084a52d7a93cd65ccf
Jonathan <jkd6735@gmail.com>
2021-07-21 17:44:06 +0000

    commit to main

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let city = {
-  name: "Madrid",
-  country: "Spain"
+function Jonathan (
+  {library, 
+    message, 
+    number}) {
+  return (
+    <div>
+      <h1>It's me, Jonathan</h1>
+      <p>I am going to be the best developer in the world</p>
+      <h2>Jonathan is currently learning {library}</h2>
+      <p>{message} at {number}</p>
+    </div>
+  )
 }
+
+
 ReactDOM.render(
-  <h1>{city.name} is in {city.country}!</h1>,
+  <Jonathan library = "React" message = "Have fun" number = {5}/>,
   document.getElementById('root')
 );
