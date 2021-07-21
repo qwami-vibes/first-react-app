5a83ce4fdc8b722d864f958af7d7d37e64f50fd4
Kwaame Ofori-Adjekum <44780028+qwami-vibes@users.noreply.github.com>
2021-07-21 23:10:22 +0000

    Make changes to the app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
-// import logo from './logo.svg';
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <p>I want it show in the main file</p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-function Jonathan ({library, message, number}) {
+function App({message, library, number}) {
   return (
-    <div>
-      <h1>It's me, Jonathan</h1>
-      <p>I am going to be the best developer in the world</p>
-      <h2>Jonathan is currently learning {library}</h2>
-      <p>{message} at {number}</p>
-      
+    <div className="App">
+      <div>
+        <h1>It's me, Jonathan</h1>
+        <p>I am going to be the best developer in the world</p>
+        <h2>Jonathan is currently learning {library}</h2>
+        <p>{message} at {number}</p>
+      </div>
     </div>
   );
 }
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Jonathan library = "React" message = "Have fun" number = {5}/>
-  </React.StrictMode>
-  ,
-  document.getElementById('root')
-);
 
 export default Jonathan;
